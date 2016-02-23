17a0cbc961ddedf5aaaae23ca407576fab9dd651
Мессир <@192.168.0.100>
2016-02-23 14:55:13 +0200

    test

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of index
+ *
+ * @author Мессир
+ */
+class index {
+
+//put your code here
+    public function der() {
+        echo 'hello world';
+    }
+
+}
